02274985a8f53591f4e232cf89a2f78b6b6b8808
YounusMontasir <ymontasir73@gmail.com>
2024-09-23 22:43:24 +0600

    blog to home issue solved

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,6 @@
 document.getElementById("blog-section").addEventListener('click',function(){
     window.location.href = "../blog.html"
 })
+document.getElementById("home-section").addEventListener('click',function(){
+    window.location.href = "index.html"
+})
