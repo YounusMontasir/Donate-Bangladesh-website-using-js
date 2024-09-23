81be97acdc187d5abf57ec5f817ad1754728ea3c
YounusMontasir <ymontasir73@gmail.com>
2024-09-23 22:56:58 +0600

    blog section half complete

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,4 @@
 document.getElementById("blog-section").addEventListener('click',function(){
     window.location.href = "../blog.html"
 })
-document.getElementById("home-section").addEventListener('click',function(){
-    window.location.href = "index.html"
-})
+
