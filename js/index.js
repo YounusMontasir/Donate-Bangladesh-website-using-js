3bc54dd7fdd682a77c2f9f62c0ca04933e3b6d21
YounusMontasir <ymontasir73@gmail.com>
2024-09-24 13:17:49 +0600

    js in one function

--- js/index.js
+++ js/index.js
@@ -10,13 +10,20 @@ function getInnerTextById (id){
     const innerValue = document.getElementById(id).innerText
     return innerValue;
 }
+// places name object
+    const places = {
+        1:"Flood at Noakhali, Bangladesh",
+        2: "famine-2024 at Feni, Bangladesh",
+        3: "Aid for Injured in the Quota Movement, Bangladesh"
+
+    }
 
 // card 1 functionality
-const donationButton1 = document.getElementById('donate-btn1')
-    donationButton1.addEventListener('click',function(){
-    const donationAmount = parseFloat(getValueFromInput('donation-amount-1'))
+
+    function cardFunctionality(cardNumber){
+    const donationAmount = parseFloat(getValueFromInput(`donation-amount-${cardNumber}`))
     const currentBalance = parseFloat(getInnerTextById("total-amount"))
-    const donationBalannce = parseFloat(getInnerTextById("donation-balance1"))
+    const donationBalannce = parseFloat(getInnerTextById(`donation-balance${cardNumber}`))
     
     if(isNaN(donationAmount) || donationAmount <= 0)
     {
@@ -34,94 +41,27 @@ const donationButton1 = document.getElementById('donate-btn1')
     }
     
     
-    document.getElementById("donation-balance1").innerText = totalDonatedAmount;
+    document.getElementById(`donation-balance${cardNumber}`).innerText = totalDonatedAmount;
     document.getElementById("total-amount").innerText = remainingBalance
     // history element
     const historyElement = document.createElement('div')
     historyElement.className = "border border-gray-300 rounded-xl  p-8 mb-4"
     historyElement.innerHTML = `
-        <p class = "text-xl font-bold">${donationAmount} Taka is Donate for Flood at Noakhali, Bangladesh</p>
+        <p class = "text-xl font-bold">${donationAmount} Taka is Donate for 
+        ${places[cardNumber]}</p>
         <p class= "opacity-70 font-light mt-4">${new Date().toLocaleString()}</p>
     `
     document.getElementById("history-section").appendChild(historyElement)
-    document.getElementById("my_modal_1").showModal();
-    document.getElementById("donation-amount-1").value = ""
+    document.getElementById(`my_modal_${cardNumber}`).showModal();
+    document.getElementById(`donation-amount-${cardNumber}`).value = ""
 
-})
+}
+const cardNumber = 3;
+for(let i=1;i<=cardNumber;i++){
+    const donation = document.getElementById(`donate-btn${i}`)
+    donation.addEventListener('click',function(){
+        cardFunctionality(i);
+    })
+}
 
-// card 2 funtionality
-const donationButton2 = document.getElementById('donate-btn2')
-    donationButton2.addEventListener('click',function(){
-    const donationAmount = parseFloat(getValueFromInput('donation-amount-2'))
-    const currentBalance = parseFloat(getInnerTextById("total-amount"))
-    const donationBalannce = parseFloat(getInnerTextById("donation-balance2"))
-    
-    if(isNaN(donationAmount) || donationAmount <= 0)
-    {
-        alert("Invalid number")
-        return
-    }
-
-
-//    amount donate for feni
-    const  totalDonatedAmount= donationAmount + donationBalannce;
-    const remainingBalance = currentBalance - donationAmount
-    if(donationAmount > currentBalance ){
-        alert("You have insuffuicient balance")
-        return
-    }
-    
-    
-    document.getElementById("donation-balance2").innerText = totalDonatedAmount;
-    document.getElementById("total-amount").innerText = remainingBalance
-    // history element
-    const historyElement = document.createElement('div')
-    historyElement.className = "border border-gray-300 rounded-xl  p-8 mb-4"
-    historyElement.innerHTML = `
-        <p class = "text-xl font-bold">${donationAmount} Taka is Donated for famine-2024 at Feni, Bangladesh</p>
-        <p class= "opacity-70 font-light mt-4">${new Date().toLocaleString()}</p>
-    `
-    document.getElementById("history-section").appendChild(historyElement)
-    document.getElementById("my_modal_2").showModal();
-    document.getElementById("donation-amount-2").value = ""
-
-})
-
-// card 3 functionality
-const donationButton3 = document.getElementById('donate-btn3')
-    donationButton3.addEventListener('click',function(){
-    const donationAmount = parseFloat(getValueFromInput('donation-amount-3'))
-    const currentBalance = parseFloat(getInnerTextById("total-amount"))
-    const donationBalannce = parseFloat(getInnerTextById("donation-balance3"))
-    
-    if(isNaN(donationAmount) || donationAmount <= 0)
-    {
-        alert("Invalid number")
-        return
-    }
-
-
-//    amount donate for noakhali
-    const  totalDonatedAmount= donationAmount + donationBalannce;
-    const remainingBalance = currentBalance - donationAmount
-    if(donationAmount > currentBalance ){
-        alert("You have insuffuicient balance")
-        return
-    }
-    
-    
-    document.getElementById("donation-balance3").innerText = totalDonatedAmount;
-    document.getElementById("total-amount").innerText = remainingBalance
-    // history element
-    const historyElement = document.createElement('div')
-    historyElement.className = "border border-gray-300 rounded-xl  p-8 mb-4"
-    historyElement.innerHTML = `
-        <p class = "text-xl font-bold">${donationAmount} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
-        <p class= "opacity-70 font-light mt-4">${new Date().toLocaleString()}</p>
-    `
-    document.getElementById("history-section").appendChild(historyElement)
-    document.getElementById("my_modal_3").showModal();
-    document.getElementById("donation-amount-3").value = ""
-
-})
 
